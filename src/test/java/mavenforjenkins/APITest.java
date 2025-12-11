55af7214329152c77e08fcf6c4009b59fcfee282
Sakthibps <91511377+Sakthibps@users.noreply.github.com>
2025-12-11 12:00:11 +0530

    SCRUM-3 Install the visual studio

--- src/test/java/mavenforjenkins/APITest.java
+++ src/test/java/mavenforjenkins/APITest.java
@@ -8,6 +8,7 @@ public class APITest {
 		System.out.println("Hi");
                  System.out.println("Good Morning");
 		 System.out.println("Jira commit changes");
+		System.out.println("Jira new commit");
                 System.out.println("Bye!!!");
 	}
 	
